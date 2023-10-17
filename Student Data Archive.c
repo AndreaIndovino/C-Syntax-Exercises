295b69520ecde3a1f0c74073a733034289d5df64
Andrea Indovino <andrea.indovino1994@gmail.com>
2023-10-17 09:35:26 +0200

    Add files via upload

---
+++ Student Data Archive.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int studentID = 15;
+int studentAge = 23;
+float studentFee = 75.25;
+char studentGrade = 'B';
+
+int main(){
+    printf("Student ID is: %d\n", studentID);
+    printf("Student age is: %d\n", studentAge);
+    printf("Student fee is: %f\n", studentFee);
+    printf("Student grade is: %c\n", studentGrade);
+    return 0;
+}
---
+++ myfirstproject.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char myChar = 'A';
+int myNum = 29;
+float myFloat = 15.5;
+
+int main(){
+    printf("%c\n\n", myChar);
+    printf("%d\n\n", myNum);
+    printf("%f\n\n", myFloat);
+    printf("My name's letter is %c\n\n", myChar);
+    printf("My age is %d\n\n", myNum);
+    printf("My float number is: %f\n", myFloat);
+    return 0;
+}
